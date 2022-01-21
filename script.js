c9bb20b642c1c2509794795bf601bd4a54b7d65c
Matias-Montes <tute.1986@gmail.com>
2022-01-21 19:22:09 -0300

    agrego funcionalidades

--- script.js
+++ script.js
@@ -18,3 +18,60 @@ No se permite acentuación de palabras
 Solo letras minusculas
 No se permite acentuación de palabras   
 */
+
+
+var frase = document.querySelector("#input-texto");
+var mensaje = document.querySelector("#msg");
+var botonEncriptar = document.querySelector("#btn-encriptar");
+var botonDesencriptar = document.querySelector("#btn-desencriptar");
+var botonCopiar = document.querySelector("#btn-copy");
+var caracteresProhibidos="ABCDEFGHYJKLMNÑOPQRSTUVWXYZáéíóúÁÉÍÓÚ!#$%&'()*+,-.\\/:;=?@[\"]^_`{|}~";
+
+
+
+
+
+function tiene_mayusculas(palabra){
+    for(i=0; i<palabra.length; i++){
+       if (caracteresProhibidos.indexOf(palabra.charAt(i),0)!=-1){
+          return alert("El texto tiene un caracter invalido");
+          break;
+       }
+    }
+    const encriptado = palabra.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    mensaje.value = encriptado;
+}
+
+
+botonEncriptar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var palabra = frase.value;
+
+    tiene_mayusculas(palabra)
+});
+
+
+botonDesencriptar.addEventListener("click", function(event){
+
+  event.preventDefault();
+  
+  var palabra = frase.value; 
+
+  const desencriptado = palabra.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+  mensaje.value = desencriptado;
+
+});
+
+    botonCopiar.addEventListener("click", function(event){
+        event.preventDefault();
+    
+        var content = mensaje;
+          
+        content.select();
+        document.execCommand('copy');
+      
+        alert("Mensaje Copiado!")
+      
+})
+
